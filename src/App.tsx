7907e33f282f94aae09010a84450c86fccf59186
Thiago Ribeiro <thiagoribo@gmail.com>
2026-02-23 21:20:27 -0300

    feat: add Vercel Speed Insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -55,6 +56,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
+          <SpeedInsights />
           <BrowserRouter>
             <ScrollToTop />
             <Routes>
